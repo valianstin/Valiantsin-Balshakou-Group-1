5aa914b3939ea3bcd4db2727494513b21d2c283f
NazarMaslenchanka <Nazar.maslenchanka@epam.com>
2014-10-05 19:07:35 +0300

    test

--- Classloader task/src/com/epam/classloader/task/App.java
+++ Classloader task/src/com/epam/classloader/task/App.java
@@ -1,9 +1,20 @@
 package com.epam.classloader.task;
+//Create console application for dynamic reloading of existing development functionality. 
+//Existing funcionality should be placed in specifed directory and should be archived in jar. 
+//The application should have console menu for choosing option, the output should be done through usage of log4j library. 
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (; ; ) {
+            ClassLoader loader = new TestClassLoader();
+            Class clazz = Class.forName("com.epam.modules.TestModuleImpl", true, loader);
+            TestModule object = (TestModule) clazz.newInstance();
+            logger.info(object);
+            TestModuleWrapper wrapper = new TestModuleWrapper(object);
+            wrapper.hello();
+            new BufferedReader(new InputStreamReader(System.in)).readLine();
+        }
 
 	}
 
